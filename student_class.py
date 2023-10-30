2b47f5e0755ba532492e6d284d0bf162f62bda1d
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2023-10-30 22:52:26 +0530

    Added class

---
+++ student_class.py
@@ -0,0 +1,8 @@
+class student:
+    def __init__(self, name, roll_no, age):
+        self.roll_no = roll_no
+        self.age = age
+        self.name = name
+    
+student1 = student("Jihn", 10, 18)
+print(student1  .name)
