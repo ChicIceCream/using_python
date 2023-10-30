28b34d185dc0dbe11d2e1746ac30a689fea2d819
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2023-10-30 23:01:24 +0530

    added for loop for no reason

--- student_class.py
+++ student_class.py
@@ -5,4 +5,9 @@ class student:
         self.name = name
     
 student1 = student("Jihn", 10, 18)
-print(student1  .name)
+print(student1.name)
+print(student1.roll_no)
+print(student1.age)
+
+for i in range(9):
+    print(i)
