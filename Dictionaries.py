22cdf109fac2a6f425e044eb1e6cbaca8496a2e1
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2023-11-06 19:00:49 +0530

    fninished tuples

---
+++ Dictionaries.py
@@ -0,0 +1,13 @@
+# DICTIONARIES HAAHAAHAH
+
+mydict = {"name" : "Max", "age" : 28, "city" : "Indore"}
+print(mydict)
+
+mydict2 = dict(name="Max", age=28, city="Indore")
+print(mydict2)
+
+print_name = mydict["name"]
+print(print_name)
+
+mydict["email"] = "yessir@gmail.com"
+print(mydict)
--- Tuples.py
+++ Tuples.py
@@ -2,3 +2,9 @@
 
 mytuple = ("Boston", "Yya", 8)
 print(mytuple)
+
+city_name, gibberish, number = mytuple
+
+print(city_name)
+print(gibberish)
+print(number) 
