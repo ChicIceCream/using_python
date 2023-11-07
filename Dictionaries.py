0a14166cb3bb430becb4652946b88b5a592bed71
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2023-11-07 17:22:11 +0530

    learning more about dictionaires through gpt

--- Dictionaries.py
+++ Dictionaries.py
@@ -1,15 +1,27 @@
 # DICTIONARIES HAAHAAHAH
 
-mydict = {"name" : "Max", "age" : 28, "city" : "Indore"}
-print(mydict)
+mydict = {
+    "name" : "Max", 
+    "age" : 28, 
+    "city" : "Indore"
+}
+# print(mydict)
 
-mydict2 = dict(name="Max", age=28, city="Indore")
-print(mydict2)
+# mydict2 = dict(name="Max", age=28, city="Indore")
+# print(mydict2)
 
-print_name = mydict["name"]
-print(print_name)
+# print_name = mydict["name"]
+# print(print_name)
 
-mydict["email"] = "yessir@gmail.com"
-print(mydict)
-mydict.popitem()
-print(mydict)
+# mydict["email"] = "yessir@gmail.com"
+# print(mydict)
+# mydict.popitem()
+# print(mydict)
+
+if "name" in mydict:
+    print("Max is in the dictionary")
+else:
+    print("Max is not in the dictionrary!")
+    
+for key, value in mydict.items():
+    print(key, value)
