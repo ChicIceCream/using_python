df40fae91053d02cdce8ca6950e7421b908408ea
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2023-11-09 17:55:35 +0530

    finished sets from vid

---
+++ sets.py
@@ -0,0 +1,17 @@
+# SETSSSSS
+
+# fruits = {"apple", "cherry", "orrange"}
+# print(fruits)
+
+# string = str(input("Give me a few words."))
+
+# set1 = set(string)
+# print(len(set1))        this will print the number of leeters in the set
+
+setA = {1,2,3,8,9,10}
+setB = {1,2,3,4,5,6}
+
+setA.update(setB)
+print(setA)
+
+print(setB.issubset(setA))
