5d8979de04c00d17a14b7cbc700eae1f00b814fc
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2023-11-13 03:13:12 +0530

    learned randoming

---
+++ NUMPY.py
@@ -0,0 +1,6 @@
+#NUMPYYYYY
+
+import numpy as np 
+
+a = np.random.rand(3)
+print(a)
---
+++ random numberss.py
@@ -0,0 +1,21 @@
+# # RANDOMING!!!
+
+import random
+
+# a = random.random()
+# print(a)
+
+# a = random.uniform(1,10)
+# print(a)
+
+# a = random.randint(1,10) # includes the upper bound
+# b = random.randrange(1,10) # does NOT include the upper bound
+# print(a,b)
+
+mylist = list("ABCDEFG")
+print(mylist)
+c = random.choice(mylist)
+print(c)
+
+c = random.sample(mylist, 3) # prints numerous random values
+print(c)
